732d37814a32f24697ea9ca0ffe40eb2e5014b5d
fishr01 <chronkillerza@gmail.com>
2014-10-15 22:24:06 +0200

    Clean up old code

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-
-/*224dda2ca82558ef0e550aa711aae69c
-
-var Request = new XMLHttpRequest();
-
-Request.open('GET', 'http://private-anon-83640780d-themoviedb.apiary-mock.com/3/discover/movie', false);
-
-Request.setRequestHeader('Accept', 'application/json');
-
-Request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Status:', this.statusText);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-  }
-};
-
-Request.send();
-*/
 function getSimilar(id){
 	   jQuery("#results").empty();
     var url = 'https://api.themoviedb.org/3/movie/' + id +'/similar?api_key=224dda2ca82558ef0e550aa711aae69c';
